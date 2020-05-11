c93235dc9f0a71fa907ea2f16c2aca8d28411ace
Marharyta Sirachenko <sirachenko944@gmail.com>
2020-05-11 23:10:51 +0200

    Zadanie 1

---
+++ zadanie1 .py
@@ -0,0 +1,33 @@
+'''Prostokąt
+
+a = float(input())
+b = float(input())
+
+print(a*b) #pole
+print(2*a+2*b) #obwód
+
+#Trójkąt
+
+a = float(input())
+b = float(input())
+c = float(input())
+print(a+b+c) #obwód
+
+a = float(input())
+h = float(input())
+print((a*h)/2) #pole
+
+#Kwadrat
+
+a = float(input())
+
+print(a*a) #pole
+print(a*4) #obwód
+
+#Koło
+a = float(input())
+
+print(3.14*(a**2)) #pole
+print((2*3.14)*a) #obwod'''
+
+
